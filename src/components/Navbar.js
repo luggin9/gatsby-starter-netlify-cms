0b7c0a8aee17efafb40a75c830ed23385b96ca44
luggin9 <48134756+luggin9@users.noreply.github.com>
2019-03-28 20:38:28 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={logo} alt="Kaldi" style={{ width: '14em', height: '10em' }} />
             </Link>
             {/* Hamburger menu */}
             <div
