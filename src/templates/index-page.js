ecbb19370d32b915a7e301bab60acb7a387a7963
luggin9 <48134756+luggin9@users.noreply.github.com>
2019-03-28 20:28:08 -0400

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -30,8 +30,8 @@ export const IndexPageTemplate = ({
       <div
         style={{
           display: 'flex',
-          height: '350px',
-          lineHeight: '3',
+          height: '150px',
+          lineHeight: '1',
           justifyContent: 'space-around',
           alignItems: 'left',
           flexDirection: 'column',
